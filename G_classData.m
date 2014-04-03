70050a94f8f637ece69220a8468da8a82d20ddd9
JesperLH <jesper.l@hinrich.dk>
2014-04-03 22:27:38 +0200

    Tilføjet 'logistic' regression, fixed randperm, + minior additions

--- G_classData.m
+++ G_classData.m
@@ -1,12 +1,14 @@
-clear
-close
+clear all
+close all
 clc
 
 p = 2; % number of dimensions
 N = 1000; % number of datapoints
-type = 'GA';
+r = 100; % sample size
+type = 'T3';
+clusterDistribution = 0.5;
 
-distance = 1.3; % manual konstant
+distance = 0.9; % manual konstant
 
 %% Covariance matrix
 
@@ -32,7 +34,7 @@ switch type
         X = mvtrnd(sigma,3,N); %T dist Data with zero mean
 end
 
-N1 = round(N/2);
+N1 = round(N*clusterDistribution);
 N2 = N-N1;
 
 D = [repmat(-D,N1,1);repmat(D,N2,1)];
@@ -42,14 +44,38 @@ X = X+D;
 t = [-ones(N1,1); ones(N2,1)];
 
 
-plot(X(:,1),X(:,2),'.')
+if (size(X,2) == 2)
+    figure
+    plot(X(1:N1,1),X(1:N1,2),'.b',X(N2+1:N,1),X(N2+1:N,2),'.r')
+    legend('Class 1', 'Class 2');
+    title(sprintf('Simulated data\n %s distribution',type));
+end
 
 %% Random permutation for more realism
 
 R = randperm(N);
 
-X = X(R);
+X = X(R,1:2);
 t = t(R);
 
+%% "Logistic" regression
 
+%The "leverage" distribution
+%Same as for linear regression, but multiplied by targets (t)
+H = X*inv(X'*X)*X'; 
 
+%figure
+%bar(sort(diag(H).*t));
+
+%Finding the distribution, we take abs((H)
+pi = abs(diag(H))./sum(abs(diag(H)));
+if (p == 2) 
+    [ w_est, Err , w_estH , ErrH ] = SubsampleLogReg( X,t,pi,r , true);
+    [ w_estU, ErrU , w_estHU , ErrHU ] = SubsampleLogReg(X,t,ones(N,1)/N,r,true);
+else
+    %SubsampleLogReg(X,t,pi,r,false); %Leverage
+    [ w_est, Err , w_estH , ErrH ] = SubsampleLogReg( X,t,pi,r , false);
+    [ w_estU, ErrU , w_estHU , ErrHU ] = SubsampleLogReg(X,t,ones(N,1)/N,r,false);
+end
+[Err ErrH]
+[ErrU ErrHU]
