aaf39ee3d7b99ed32baf05041b2e2c0bb2fff8d9
JesperLH <jesper.l@hinrich.dk>
2014-05-19 14:22:51 +0200

    Flere flotte plots

--- showcaseData.m
+++ showcaseData.m
@@ -2,33 +2,43 @@
 close all
 titleSize = 32
 % Ga
-N = 50000;
-p = 1;
+N = 1000;
+p = 2;
+yaxis = [-10 10];
+xaxis = [-50 50];
 type = 'GA';
 [X,y] = generateData(N,p,type);
+X = (X-repmat(mean(X),N,1))./repmat(sqrt(var(X)),N,1);
 subplot(1,3,1)
-hist(X,50)
+%hist(X,50)
+plot(X(:,1),X(:,2),'ob');
 title('GA','fontweight','bold','fontsize',titleSize);
-ylim([0 5*10^4])
+ylim(yaxis)
+xlim(xaxis)
 set(gca, 'xcolor', [0 0 0],'ycolor', [0 0 0],'color', 'none');
 
 % T3
 type = 'T3';
 [X,y] = generateData(N,p,type);
+X = (X-repmat(mean(X),N,1))./repmat(sqrt(var(X)),N,1);
 subplot(1,3,2)
-hist(X,50)
+%hist(X,50)
+plot(X(:,1),X(:,2),'ob');
 title('T3','fontweight','bold','fontsize',titleSize);
-ylim([0 5*10^4])
+ylim(yaxis)
+xlim(xaxis)
 set(gca, 'xcolor', [0 0 0],'ycolor', [0 0 0],'color', 'none');
 
 % T1
 
 type = 'T1';
 [X,y] = generateData(N,p,type);
+X = (X-repmat(mean(X),N,1))./repmat(sqrt(var(X)),N,1);
 subplot(1,3,3)
-hist(X,50)
+%hist(X,50)
+plot(X(:,1),X(:,2),'ob');
 title('T1','fontweight','bold','fontsize',titleSize);
-
-ylim([0 5*10^4])
+xlim(xaxis)
+ylim(yaxis) 
 set(gca, 'xcolor', [0 0 0],'ycolor', [0 0 0],'color', 'none');
 set(gcf, 'color', 'none','inverthardcopy', 'off');
